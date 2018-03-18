6d54f254e8d44febc8cdf2825a0e0aa3d787c13c
DeegiiAlt <aagiideegii@gmail.com>
2018-03-17 20:25:41 -0600

    contact form

--- logic.js
+++ logic.js
@@ -1,1 +1,26 @@
-$(".contactform").val()
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAjG86f0kX1pmdcmYA5XbbFJMoULMRqlrQ",
+	authDomain: "portfolio-contact-71957.firebaseapp.com",
+	databaseURL: "https://portfolio-contact-71957.firebaseio.com",
+	projectId: "portfolio-contact-71957",
+	storageBucket: "",
+	messagingSenderId: "155461234441"
+};
+
+firebase.initializeApp(config);
+
+//Grab values from contact form
+var name = $("#nameInput").val().trim();
+var email = $("#emailInput").val().trim();
+var message = $("#messageInput").val().trim();
+
+$("#submitButton").click(function(event) {
+	event.preventDefault();
+
+	database.ref().set({
+		name: name,
+		email: email,
+		message: message
+	});
+});
